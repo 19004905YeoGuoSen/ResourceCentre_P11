01bf0821f514610e3a2d8317833c8a97c6af6131
19005146-Mok-SiewTheng <19005146@myrp.edu.sg>
2020-08-05 12:25:39 +0800

    Moved showAvailability() into Item, Update toString()

--- ResourceCentre_P11/src/Item.java
+++ ResourceCentre_P11/src/Item.java
@@ -11,7 +11,7 @@ public class Item {
 		this.dueDate = "";
 		this.isAvailable = true;
 	}
-	
+
 	public String getAssetTag() {
 		return assetTag;
 	}
@@ -35,7 +35,7 @@ public class Item {
 	public void setIsAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
-	
+
 	public static String showAvailability(boolean isAvailable) {
 		String avail;
 
@@ -48,9 +48,9 @@ public class Item {
 	}
 
 	public String toString() {
-		
-		return String.format("%-10s %-30s %-10s %-10s\n", getAssetTag(), getDescription(), showAvailability(getIsAvailable()), getDueDate());
 
+		return String.format("%-10s %-30s %-10s %-10s\n", getAssetTag(), getDescription(),
+				showAvailability(getIsAvailable()), getDueDate());
+	}
 
 }
-
