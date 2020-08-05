784b0c3114f292f76325866e524626aec2b14430
Owner <Owner@Kyoshi>
2020-08-05 12:53:32 +0800

    Updated retrieveAllChromebook line 151

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -148,10 +148,7 @@ public class ResourceCentre {
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					Item.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
+			output += String.format("%-84s", chromebookList.get(i).toString());
 		}
 		return output;
 	}
